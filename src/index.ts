a573c06e5d1229c5ad4f949065243138abf90f84
Marshal Wu <marshal.wu@gmail.com>
2022-03-08 16:12:21 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import { program } from "commander";
+
 export const Hello = (name: string) => `Hello ${name}`;
+
+program.option("--first").option("-s, --separator <char>");
+
+program.parse();
+
+console.log("fake ts service OK.");
